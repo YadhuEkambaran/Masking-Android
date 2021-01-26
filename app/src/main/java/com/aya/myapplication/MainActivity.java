a751ae3c20df3d567e0b7bf309ab66a72767c1fd
Yadhukrishnan Ekambaran <yadhu@tazwiz.com>
2021-01-25 22:29:21 -0500

    Bug fixed Compatible to format phone numbers

--- app/src/main/java/com/aya/myapplication/MainActivity.java
+++ app/src/main/java/com/aya/myapplication/MainActivity.java
@@ -3,9 +3,7 @@ package com.aya.myapplication;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputFilter;
-import android.text.InputType;
 import android.text.SpannableStringBuilder;
-import android.text.Spanned;
 import android.text.TextWatcher;
 import android.text.method.DigitsKeyListener;
 import android.util.Log;
@@ -15,13 +13,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String PATTERN = "(###) ### ####";
+    private static final String PATTERN = "##/##";
 
     private static final String DIGITS_FOR_TEXT = "0123456789 abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static final String DIGITS_FOR_NUMBER = "0123456789() ";
+    private static final String DIGITS_FOR_NUMBER = "0123456789 ";
 
-    private boolean isBackPressed = false;
-    private boolean isForwardFormatting = false;
+    private int prevLength = 0;
 
     private EditText editText;
 
@@ -40,16 +37,13 @@ public class MainActivity extends AppCompatActivity {
 
     private final TextWatcher textWatcher = new TextWatcher() {
         @Override
-        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
 
         }
 
         @Override
-        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            if (isForwardFormatting) {
-                formatter(charSequence);
-            }
-
+        public void onTextChanged(CharSequence charSequence, int start, int after, int count) {
+            formatter(charSequence, start);
         }
 
         @Override
@@ -67,102 +61,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private final InputFilter inputFilter = (charSequence, start, end, dest, dStart, dEnd) -> {
-        Log.d(TAG, "--- charSequence -- " + charSequence);
-        Log.d(TAG, "--- start -- " + start);
-        Log.d(TAG, "--- end -- " + end);
-        Log.d(TAG, "--- dest -- " + dest);
-        Log.d(TAG, "--- dStart -- " + dStart);
-        Log.d(TAG, "--- dEnd -- " + dEnd);
+
         if (charSequence.toString().matches("[ ,.]")) {
             return "";
         }
 
-        if (editText.getInputType() == InputType.TYPE_CLASS_TEXT) {
-            if ((end - start) > (dEnd - dStart)) {
-                return textFormatter(charSequence, dest.toString(), dEnd);
-            } else {
-                return null;
-            }
-        } else {
-            if ((start == 0 && end == 0)) {
-                if (dStart < editText.getText().length()) {
-                    numberBackFormatter(dStart, dEnd);
-                }
-                return null;
-            } else if (((dStart - dEnd) == 0)) {
-                if (charSequence.length() == 1) {
-                    return numberFormatter(charSequence, dStart);
-                }
-            }
-
-            return null;
-        }
+        return null;
     };
 
-    private CharSequence textFormatter(CharSequence charSequence, String dest, int charPosition) {
-        int lastPosition = dest.lastIndexOf(" ");
-        if (lastPosition != -1) {
-            dest = dest.substring(lastPosition + 1);
-        }
-
-        SpannableStringBuilder builder = new SpannableStringBuilder(dest);
-        Log.d("-- textFormatter --", "-- DEST -- " + dest);
-        Log.d("-- textFormatter --", "-- charSequence -- " + charSequence);
-        while (PATTERN.charAt(charPosition) != '#') {
-            builder.append(PATTERN.charAt(charPosition));
-            charPosition = charPosition + 1;
-        }
-        builder.append(charSequence.charAt(charSequence.length() - 1));
-        return builder.toString().toUpperCase();
-    }
-
-    private CharSequence numberFormatter(CharSequence charSequence, int charPosition) {
-        SpannableStringBuilder builder = new SpannableStringBuilder();
-        while (PATTERN.charAt(charPosition) != '#') {
-            char letter = PATTERN.charAt(charPosition);
-            builder.append(letter);
-            ++charPosition;
-        }
-
-        if (isBackPressed) {
-            isForwardFormatting = true;
-            isBackPressed = false;
-        }
-        return builder.append(charSequence);
-    }
-
-    private void numberBackFormatter(int dStart, int dEnd) {
-        char[] patternChars = PATTERN.toCharArray();
-
-        SpannableStringBuilder charBuilder = new SpannableStringBuilder(editText.getText().toString());
-        charBuilder.replace(dStart, dEnd, "");
-        String formattingText = charBuilder.toString().replaceAll("[^0-9]", "");
-        if (formattingText.length() <= 0) {
-            return;
-        }
-        SpannableStringBuilder builder = new SpannableStringBuilder();
-        int patternPosition = 0;
-        for (int i = 0; i < formattingText.length(); i++) {
-            char textChar = formattingText.charAt(i);
-            if (isInt(textChar)) {
-                char patternChar = patternChars[patternPosition++];
-                if (patternChar != '#') {
-                    builder.append(patternChar);
-                    ++patternPosition;
-                }
-                builder.append(textChar);
-            }
-        }
-
-        removeTextWatcher();
-        editText.setText(builder);
-        int position = dStart > builder.length() ? dStart - 1 : dStart;
-        editText.setSelection(position);
-        isBackPressed = true;
-        addWatcher();
-    }
-
-    private void formatter(CharSequence sequence) {
+    private void formatter(CharSequence sequence, int start) {
         char[] patternChars = PATTERN.toCharArray();
 
         SpannableStringBuilder charBuilder = new SpannableStringBuilder(sequence);
@@ -176,19 +83,40 @@ public class MainActivity extends AppCompatActivity {
             char textChar = formattingText.charAt(i);
             if (isInt(textChar)) {
                 char patternChar = patternChars[patternPosition++];
-                if (patternChar != '#') {
+                while (patternChar != '#') {
                     builder.append(patternChar);
-                    ++patternPosition;
+                    patternChar = patternChars[patternPosition++];
                 }
                 builder.append(textChar);
             }
         }
 
-        isForwardFormatting = false;
+        int position = builder.length();
+        if (prevLength > builder.length()) {
+            position = getPosition(start);
+        }
+
+        prevLength = builder.length();
         removeTextWatcher();
         editText.setText(builder);
-        editText.setSelection(builder.length());
+        editText.setSelection(position);
+
         addWatcher();
+
+
+        Log.d(TAG, "--- LENGTH ---" + builder.length());
+    }
+
+    private int getPosition(int position) {
+        char[] patternChars = PATTERN.toCharArray();
+        if (position == 0 || position == 1) {
+            return position;
+        }
+
+        if (patternChars[position - 1] == '#') {
+            return position;
+        }
+        return getPosition(--position);
     }
 
     private boolean isInt(char number) {
